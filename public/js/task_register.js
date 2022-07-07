6a36fefab22fbb8eba09285e809938b9f5162c06
y92-yuki <yukiyamanaka92@gmail.com>
2022-07-07 22:58:38 +0900

    通知時間の表示を制御

--- public/js/task_register.js
+++ public/js/task_register.js
@@ -2,7 +2,20 @@
 
 window.addEventListener('DOMContentLoaded',() => {
 
+    //現在時刻を取得
     const now_date = new Date();
+    const notification_at = document.querySelectorAll('.notification_at');
+
+    //指定している通知時間を表示して、通知する時間のものは赤文字にする
+    for (let p of notification_at) {
+        if (now_date.getTime() >= Date.parse(p.textContent)) {
+            p.parentNode.classList.remove('d-none');
+            p.parentNode.classList.add('text-danger');
+        } else if (p.textContent) {
+            p.parentNode.classList.remove('d-none');
+        }
+    }
+
     // 現在時刻が18時以降の場合は翌日が初期値
     if (now_date.getHours() > 17 && now_date.getHours() < 23) {
         now_date.setDate(now_date.getDate() + 1);
@@ -16,12 +29,21 @@ window.addEventListener('DOMContentLoaded',() => {
     const date = String(now_date.getDate()).padStart(2,'0');
     const hour = String(now_date.getHours()).padStart(2,'0');
 
-    //通知時間を有効にチェックを入れる作動
-    document.querySelector('#apply_time_notification').onchange = () => {
-        const input_date = document.querySelector('input[name=date]');
-        const input_time = document.querySelector('input[name=time]');
+    //時間通知 有効・無効のチェックボックス
+    const apply_time_notification = document.querySelector('#apply_time_notification');
 
-        //通知時間の初期値を制御
+    //日付・時間のdiv要素
+    const time_noticication = document.querySelector('#time_notification');
+
+    //日付
+    const input_date = document.querySelector('input[name=date]');
+
+    //時間
+    const input_time = document.querySelector('input[name=time]');
+
+    //通知時間のチェックボックスイベント
+    apply_time_notification.onchange = () => {
+        
         if (input_date.value && input_time.value) {
             input_date.value = "";
             input_time.value = "";
@@ -30,9 +52,10 @@ window.addEventListener('DOMContentLoaded',() => {
             input_time.value = `${hour}:00`;
         }
 
-        document.querySelector('#time_notification').classList.toggle('d-none');
+        time_noticication.classList.toggle('d-none');
     }
 
+    //タスクの入力フォームを取得
     const task_form = document.querySelector('#task_form');
 
     task_form.onsubmit = e => {
@@ -41,12 +64,8 @@ window.addEventListener('DOMContentLoaded',() => {
         const task = document.querySelector('input[name=task]');
         const validate_message = document.querySelector('.validate_message');
 
-        for (let item of formData) {
-            console.log(item);
-        }
-
         //タスク入力欄のバリデーション
-        if (!formData.get('task').trim() && validate_message.classList.contains('d-none')) {
+        if (!formData.get('task').trim()) {
             validate_message.classList.remove('d-none');
             task.classList.add('is-invalid');
         } else {
@@ -54,12 +73,16 @@ window.addEventListener('DOMContentLoaded',() => {
                 method: 'POST',
                 body: formData
             })
-            .then(res => res.json())
+            .then(res => res.text())
+            .then(res => {})
             .then(res => {
-                console.log(res);
                 task.classList.remove('is-invalid');
                 validate_message.classList.add('d-none');
                 task.value = "";
+                input_date.value = "";
+                input_time.value = "";
+                apply_time_notification.checked = false;
+                time_noticication.classList.add('d-none');
             })
             .catch(e => {
                 console.error(e);
